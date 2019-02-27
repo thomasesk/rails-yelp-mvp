3927396774e3d1bc616b2be888a7281d2956e436
Thomas Eskenazi <thomas.eskenazi@hec.edu>
2019-02-27 14:41:11 +0100

    CRUD + Nesting de Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:destroy, :update, :edit] do
+    resources :reviews, only: [:new, :create]
+  end
 end
